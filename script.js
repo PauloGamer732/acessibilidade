1f079cf2a4f72a3fb484e7b4c3c5f342f5e4f211
PauloGamer732 <00001146228995sp@al.educacao.sp.gov.br>
2025-09-01 08:17:27 -0300

    v2

--- script.js
+++ script.js
@@ -18,10 +18,14 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
 const opcoesDeacessibilidade = document.getElementById('opcoes-acessibilidade');
+const alternaContraste = document.getElementById('alterna-contraste');
 
 botaoAcessibilidade.addEventListener('click',function()){
     botaoAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeacessibilidade.classList.toggle('apresenta-lista');
 }
-
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+}
 })
+
